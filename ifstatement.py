562a076e64c97fd0a48557e91088be583fa29391
dpdahal <dp48dahal@gmail.com>
2020-01-10 07:43:42 +0545

    first commit

---
+++ ifstatement.py
@@ -0,0 +1,31 @@
+# x = 10
+# y = 20
+#
+# if x > y:
+#     print('x')
+#
+# else:
+#     print('y')
+
+x = 100
+y = 20
+z = 30
+
+if x > y:
+    if x > z:
+        print('x')
+    else:
+        print('z')
+
+else:
+    if y > z:
+        print('y')
+    else:
+        print('z')
+
+# if x > y and x > z:
+#     print('x is large')
+# elif y > x and y > z:
+#     print('y is large')
+# else:
+#     print('z')
---
+++ introduction.py
@@ -0,0 +1,81 @@
+# print('Hello', 'python', sep='@')
+# print('today\n class')
+
+# _ = 10
+# name = 'ram'
+# age = 12
+
+# print('your name is ' + name)
+
+# print('name ',format(name))
+# print('name {} {} '.format(name, age))
+# print(f'name {name} age {age}')
+#
+# name = input("Enter any name: ")
+#
+# print(f"your name is {name}")
+
+#
+# print('mother\'s')
+# print("mother's")
+
+
+# def insert():
+#     """data insert function take two prm table name"""
+#     return "data insert"
+#
+#
+# print(insert.__doc__)
+
+
+# name = 'hari thapa'
+
+# print(dir(name))
+
+# print(name.title())
+
+# x = y = z = 50
+# print(x)
+# print(y)
+# print(z)
+
+# x = 10
+# y = 20
+# z = x
+#
+# print(x is y)
+# print(x is z)
+
+# x, y, z, = 10, 20, 30
+# print(x)
+# print(y)
+# print(z)
+
+
+# python operators
+# arithmetic operators = + - * / %
+# comparison operators = == >= <=
+# Assignment Operators =
+# Logical Operators = and or not
+# Bitwise Operators = | &
+# Membership Operators = in, not in
+# Identity Operators = is, is not
+
+# print(10 + 20)
+# print(10 - 20)
+
+# print(4 ** 2)  # exponent
+# print(4 // 3)  # floor division
+
+
+# username = input('Enter username: ')
+# password = input('Enter password: ')
+#
+# if username == 'admin' and password == 'admin':
+#     print('welcome' + username)
+#
+# else:
+#     print('no')
+
+
+# if, elif, else,for,while, break, continue
